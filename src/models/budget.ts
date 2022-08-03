f8f18607e49bed1264536acdbfd3ebaa53c30b0c
Jieun Kim <jieun.k@hotmail.com>
2022-08-02 22:01:41 -0700

    Add: user signup, login

---
+++ src/models/budget.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const getBudget = async () => {};
+const addBudget = async () => {};
+const updateBudget = async () => {};
+const deleteBudget = async () => {};
---
+++ src/models/contents.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const getContents = async () => {};
+const addContents = async () => {};
+const updateContents = async () => {};
+const deleteContents = async () => {};
